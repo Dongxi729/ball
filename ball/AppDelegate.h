95b87c990e1a2fbcc80685da6d97b7e0a546e186
zdx <18259129536@163.com>
2015-12-01 14:00:04 +0800

    Initial Commit

---
+++ ball/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ball
+//
+//  Created by 郑东喜 on 15/12/1.
+//  Copyright (c) 2015年 郑东喜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ball/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ball
+//
+//  Created by 郑东喜 on 15/12/1.
+//  Copyright (c) 2015年 郑东喜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
